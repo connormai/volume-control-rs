1a332c17f8758944b408b1c64e7a6f42016d2186
Alex <novikovprojects@gmail.com>
2022-12-07 23:02:09 +0300

    add mute function

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,18 @@ enum Commands {
 
     #[command(about = "Decrease volume by given percent")]
     Decrease { percent: f64 },
+
+    #[command(about = "Mute output")]
+    Mute,
 }
 
-fn show_volume_notification(volume: u32) {
+fn show_volume_notification(volume: u32, is_mute: bool) {
+    let vol_format = format!("{}%", volume);
+    let body = if is_mute { "Muted" } else { vol_format.as_str() };
+
     _ =  Notification::new()
        .summary(VOLUME_NOTIFICATION_SUMMARY)
-       .body(&format!("{}%", volume.to_owned()))
+       .body(body)
        .hint(Hint::CustomInt("value".to_string(), volume.try_into().unwrap()))
        .appname(VOLUME_NOTIFICATION_APPNAME)
        .timeout(VOLUME_NOTIFICATION_TIMEOUT)
@@ -46,14 +52,20 @@ fn main() {
             handler.increase_device_volume_by_percent(default_device_index, percent.to_owned()/100.0);
 
             let volume = handler.get_default_device().unwrap().volume.avg().0/655;
-            show_volume_notification(volume);
-       } 
-       Commands::Decrease { percent } => {
+            show_volume_notification(volume, false);
+        } 
+        Commands::Decrease { percent } => {
             handler.decrease_device_volume_by_percent(default_device_index, percent.to_owned()/100.0);
 
             let volume = handler.get_default_device().unwrap().volume.avg().0/655;
-            show_volume_notification(volume);
-            println!("{}", volume);
-       }
+            show_volume_notification(volume, false);
+        }
+        Commands::Mute => {
+            let is_mute = handler.get_default_device().unwrap().mute;
+            handler.set_device_mute_by_index(default_device_index, !is_mute);
+
+            let volume = handler.get_default_device().unwrap().volume.avg().0/655;
+            show_volume_notification(volume, true);
+        }
     }
 }
