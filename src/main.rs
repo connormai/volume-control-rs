a2f9d42126ceb655f895beb96d11133d988d0cfe
Alex <novikovprojects@gmail.com>
2022-12-10 17:45:10 +0300

    add icon

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use clap::{Parser, Subcommand};
 use notify_rust::{Notification, Hint};
 use pulsectl::controllers::{SinkController, DeviceControl};
@@ -27,17 +29,31 @@ enum Commands {
     Mute,
 }
 
-fn show_volume_notification(volume: u32, is_mute: bool) {
+fn show_volume_notification(volume: u32, is_mute: bool, icon_prefix: String) {
+    let volume_progress = volume%101;
+
     let vol_format = format!("{}%", volume);
     let body = if is_mute { "Muted" } else { vol_format.as_str() };
+
+    let mut icon = icon_prefix;
+    if is_mute {
+        icon += "audio-volume-muted.svg";
+    } else {
+        icon += match volume {
+            0..=33 => { "audio-volume-low.svg" }
+            34..=66 => { "audio-volume-medium.svg" }
+            67.. => { "audio-volume-high.svg" }
+        };
+    }
 
     _ =  Notification::new()
        .summary(VOLUME_NOTIFICATION_SUMMARY)
        .body(body)
-       .hint(Hint::CustomInt("value".to_string(), volume.try_into().unwrap()))
+       .hint(Hint::CustomInt("value".to_string(), volume_progress.try_into().unwrap()))
        .appname(VOLUME_NOTIFICATION_APPNAME)
        .timeout(VOLUME_NOTIFICATION_TIMEOUT)
        .id(VOLUME_NOTIFICATION_ID)
+       .icon(&icon)
        .show();
 }
 
@@ -47,25 +63,29 @@ fn main() {
     let mut handler = SinkController::create().unwrap();
     let default_device_index = handler.get_default_device().unwrap().index;
 
+    let icon_path_prefix = format!("{}/.local/share/icons/Colloid-orange-dark/status/24/", env!("HOME")); 
+
     match &cli.command {
         Commands::Increase { percent } => {
             handler.increase_device_volume_by_percent(default_device_index, percent.to_owned()/100.0);
 
             let volume = handler.get_default_device().unwrap().volume.avg().0/655;
-            show_volume_notification(volume, false);
+            show_volume_notification(volume, false, icon_path_prefix);
         } 
         Commands::Decrease { percent } => {
             handler.decrease_device_volume_by_percent(default_device_index, percent.to_owned()/100.0);
 
             let volume = handler.get_default_device().unwrap().volume.avg().0/655;
-            show_volume_notification(volume, false);
+            show_volume_notification(volume, false, icon_path_prefix);
         }
         Commands::Mute => {
             let is_mute = handler.get_default_device().unwrap().mute;
             handler.set_device_mute_by_index(default_device_index, !is_mute);
-
-            let volume = handler.get_default_device().unwrap().volume.avg().0/655;
-            show_volume_notification(volume, true);
+            
+            let new_state = handler.get_default_device().unwrap();
+            let volume = new_state.volume.avg().0/655;
+            let mute = new_state.mute;
+            show_volume_notification(volume, mute, icon_path_prefix);
         }
     }
 }
